1885a59dce5d8cb5ef427383660bbf394b441874
Naiyoma <lankas.aurelia@gmail.com>
2021-12-13 18:47:46 +0300

    total supply constructor

---
+++ contracts/qwertToken.sol
@@ -0,0 +1,11 @@
+//https://eips.ethereum.org/EIPS/eip-20
+pragma solidity ^0.8.0;
+
+contract QwertyToken {
+    uint256 totalSupply_;
+    constructor 
+    function QwertyToken () public {
+        totalSupply = 1000000;
+    }
+
+}
