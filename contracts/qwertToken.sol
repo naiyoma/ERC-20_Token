855e6d2def1916443252fac20ec5dbcc6d932246
Naiyoma <lankas.aurelia@gmail.com>
2022-01-04 23:12:08 +0300

    add balance function

--- contracts/qwertToken.sol
+++ contracts/qwertToken.sol
@@ -85,10 +85,11 @@ pragma solidity ^0.8.0;
 //       return c;
 //     }
 // }
-mapping (address => uint256) balances;
-mapping (address => mapping (address => uint256)) allowed;
+
 
 contract QwertyToken {
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
     uint public balance;
     uint256 totalSupply_;
 
