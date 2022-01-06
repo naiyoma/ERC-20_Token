de6493275eae533a3476f27a072bee561ec122a7
Naiyoma <lankas.aurelia@gmail.com>
2022-01-06 22:27:32 +0300

    test net configuration

--- contracts/qwertToken.sol
+++ contracts/qwertToken.sol
@@ -1,20 +1,7 @@
 
 contract QwertyToken {
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint public balance;
-    uint256 totalSupply_;
 
-    constructor(uint256 total) public
-    {
-        totalSupply_ = total;
-        balance = 0;
-    }
-
-    // The payable keyword allows this function to accept Eth.
-    function contribute() public payable
-    {
-        // msg.value is the value of Ether sent in a transaction
-        balance += msg.value;
+    function QwertyToken () public {
+        totalSupply = 100000;
     }
 }
