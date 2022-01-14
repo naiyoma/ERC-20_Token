a3945da5f5419d23545fc82cf9dcb5c1708cc7ab
naiyoma <lankas.aurelia@gmail.com>
2022-01-14 16:12:52 +0300

    token sale contract

---
+++ contracts/TokenSale.sol
@@ -0,0 +1,20 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./QwertyToken.sol";
+
+contract QwertTokenSale {
+    address admin;
+    QwertyToken public tokenContract;
+    uint256 public tokenPrice;
+
+    function tokenSale(QwertyToken _tokenContract, uint256 _tokenPrice) public {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+
+    }
+
+    
+}
--- migrations/2_migration.js
+++ migrations/2_migration.js
@@ -1,5 +1,9 @@
 var QwertyToken = artifacts.require("./QwertyToken.sol");
+var QwertyTokenSale = artifacts.require("./TokenSale.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(QwertyToken,1000000);
+    deployer.deploy(QwertyToken,1000000).then(function() {
+        var tokenPrice = 10000000000000;
+        return  deployer.deploy(QwertyTokenSale, QwertyToken.address, tokenPrice);
+    });
 };
