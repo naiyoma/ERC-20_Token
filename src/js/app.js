ae09efc93d0a361d6c549a3d189c02e58ed7031a
naiyoma <lankas.aurelia@gmail.com>
2022-01-19 23:28:50 +0300

    metamask connection

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,23 @@
+const { default: Web3 } = require("web3");
+
 console.log("qwerty")
 
+App = {
+    init: function() {
+        console.log("hey")
+    },
+    initWeb3: function() {
+        if (typeof web3 != 'undefined') {
+            App.web3Provider = web3.currentProvider;
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            web3 = new Web3(App.web3Provider);
+        }
+    }
+},
+$(function() {
+    $(window).load(function() {
+        App.init();
+    })
+});
