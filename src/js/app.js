0a245c06f3ac75864fdbef18936ffb1e63ba110f
naiyoma <lankas.aurelia@gmail.com>
2022-01-20 00:11:39 +0300

    initialize truffle contract

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
 App = {
     web3Provider: null,
-
+    contracts: {},
     init: function() {
         console.log("hey")
+        return App.initWeb3();
     },
     initWeb3: function() {
         if (typeof web3 !== 'undefined') {
@@ -12,9 +13,18 @@ App = {
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
             web3 = new Web3(App.web3Provider);
         }
-    }
-},
+    },
 
+    initContracts: function() {
+        $.getJSON("QwertyTokenSale.json", function(qwertyTokenSale) {
+            App.contracts.QwertyTokenSale = TruffleContract(qwertyTokenSale);
+            App.contracts.QwertyTokenSale.setProvider(App.web3Provider);
+            App.contracts.QwertyTokenSale.deployed().then(function(qwertyTokenSale){
+                console.log("initialize a cntract")
+            })
+    })
+}
+},
 
 $(function() {
     $(window).load(function() {
