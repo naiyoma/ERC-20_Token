25094d131f46f23f68e2ca6e88f4a033f536ba3c
naiyoma <lankas.aurelia@gmail.com>
2022-01-25 23:23:27 +0300

    truffle js configuration

---
+++ truffle.js
@@ -0,0 +1,16 @@
+module.exports = {
+    networks: {
+      development: {
+        host: "127.0.0.1",
+        port: "7545",
+        network_id: "*"
+      }
+    },
+    rinkeby: {
+        host: "localhost",
+        part: 8545,
+        networks_id: 4,
+        gas: 47000000
+    }
+  };
+  
