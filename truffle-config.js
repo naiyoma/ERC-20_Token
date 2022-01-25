65a926ddcc8f80524716b08a930343cbf8d05d50
naiyoma <lankas.aurelia@gmail.com>
2022-01-26 00:04:36 +0300

    configure infura

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider")
 module.exports = {
   networks: {
     development: {
@@ -10,5 +11,13 @@ module.exports = {
     solc: {
       version: "0.8.0"
     }
-  }
+  },
+  rinkeby:{
+    provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/26c6c7b4297344c6afbc26bca2f7baaa`),
+    network_id: 3,       // Ropsten's id
+    gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+  },
 };
