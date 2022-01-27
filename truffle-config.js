502c65c1ad5d5d342b6dbd90031b30b66fde709d
naiyoma <lankas.aurelia@gmail.com>
2022-01-28 01:25:35 +0300

    deploy to ropsten network

--- truffle-config.js
+++ truffle-config.js
@@ -1,41 +1,28 @@
-
+require('dotenv').config();
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const developmentMnemonic =
-"recipe flock effort test evil card identify grab shift venture movie tonight";
-const private_keys = '6c32a1dbd9023cc23501af8bea0216822c80508133ee856eab0712b67e32d2ba';
-// const infuraKey = "fj4jll3k.....";
-const MNENOMIC = "orange";
-const mnemonic = "rich";
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  // contracts_build_directory: path.join(__dirname, "./public/contracts/build/"),
   networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}` 
+      ),
+      gasPrice: 2500000000000,
+      network_id: 3
+
+    },
+  },
     development: {
       host: "localhost",
       port: 7545,
-      network_id: '*',
-      gas: 4612388,
-      gasPrice: 25000000000,
-      total_accounts: 20,
-      from: "0x0000000000000000000000000000000000000001",
-      mnemonic
+      network_id: "*"
     },
-    rinkeby: {
-      provider: () => new HDWalletProvider(
-      MNENOMIC,
-      "https://rinkeby.infura.io/v3/26c6c7b4297344c6afbc26bca2f7baaa"
-      ),
-      network_id: 4,
-      gas: 4612388,
-      gasPrice: 25000000000,
-    },
-  },
   solc: {
-    version: "0.8.0"
-
-  }
+      version: "0.8.0"
+    },
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
 };
